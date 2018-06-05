501b38ea92016c4825c9794a58bc05029f1f0659
Jacob Schopmeyer <jacob.schopmeyer@gmail.com>
2018-06-05 08:57:39 -0400

    Updated files?

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 console.log('testing')
+
+const button = document.querySelector('button')
+
+function changeCourse() {
+  const heading = document.querySelector('.spell')
+  if(heading.textContent === 'Potions'){
+      heading.textContent = 'Defense'
+  }
+  else{
+    heading.textContent = 'Potions'
+  }
+}
+function changeName(){
+  const name = document.querySelector('.name')
+  name.textContent = 'Ron'
+}
+
+
+function changeHeader(){
+  const x = document.getElementById("frm1");
+  let text = "";
+  text += x.elements[0].value
+  document.getElementById("change").innerHTML = text;
+
+}
+
+document.getElementById('first').addEventListener('click', changeCourse)
+document.getElementById('second').addEventListener('click', changeName)
+document.getElementById('third').addEventListener('click', changeHeader)
