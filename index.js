1c487e5b9e3436fb98ddd864530027eb01f04411
Jacob Schopmeyer <jacob.schopmeyer@gmail.com>
2018-06-05 18:20:52 -0400

    Added span tags and formatted the spells and levels to have different colors

--- index.js
+++ index.js
@@ -18,11 +18,33 @@ const addElements = function(ev){
   const f = ev.target;
   const spellName = f.spellName.value;
   const level = f.level.value;
-  let node = document.createElement('LI');
-  let textNode = document.createTextNode(spellName + ' - level ' + level);
 
-  node.appendChild(textNode);
-  document.getElementById("spells").appendChild(node);
+ // let brk = document.createElement('<br>');
+  
+  let spellItem = document.createElement('SPAN');
+  let levelItem = document.createElement('SPAN');
+  //let spanNode = document.createElement('SPAN', spellName)
+  //const currentSpell = document.getElementsByClassName('spellName');
+  //currentSpell.appendChild(node).appendChild(spellName).appendChild(node);
+  spellItem.className = "spellName";
+  levelItem.className = "level";
+
+  let textNode = document.createTextNode(spellName + ' - level ' + level);
+  
+  const spellNode = document.createTextNode(spellName);
+
+  const levelNode = document.createTextNode(level);
+  spellItem.appendChild(spellNode);
+  levelItem.appendChild(levelNode);
+ // node.appendChild(spanNode);
+  //node.appendChild(textNode);
+  const brk = document.createElement('br');
+  //node.appendChild(brk)
+  //document.getElementById("types").appendChild(node);
+  document.getElementById('types').appendChild(spellItem);
+  document.getElementById('types').appendChild(levelItem);
+  //document.getElementById("spellName").appendChild(spellNode);
+  
 
   f.reset();
 }
