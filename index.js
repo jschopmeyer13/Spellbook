97b9abd4ad34879d59bafdeed64de983e9ca06b9
Jacob Schopmeyer <jacob.schopmeyer@gmail.com>
2018-06-06 19:56:08 -0400

    added delete button and delete button currently just deletes the whole list

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let arr = []
 const app = {
   init: function() {
     const form = document.querySelector('form')
@@ -12,7 +13,10 @@ const app = {
     el.classList.add(name)
     return el
   },
-
+  removeProperty: function(name){
+    var element = document.getElementById(name);
+    element.parentNode.removeChild(element);
+  },
   renderItem: function(spell) {
     // ['name', 'level']
     properties = Object.keys(spell)
@@ -22,6 +26,7 @@ const app = {
     const childElements = properties.map(property => {
       return this.renderProperty(property, spell[property])
     })
+   
 
     const item = document.createElement('li')
     item.classList.add('spell')
@@ -30,20 +35,39 @@ const app = {
     childElements.forEach(el => {
       item.appendChild(el)
     })
-
+    const delButton = item.appendChild(document.createElement("button"));
+    delButton.textContent = 'delete';
+    delButton.className = 'deleteButton';
+    
+    delButton.addEventListener('click', function(){
+      const list = document.getElementById('spells');
+      
+      while(list.hasChildNodes()){
+      
+        list.removeChild(list.firstChild);
+      }
+     // item.removeChild(this);
+      //item.removeChild(document.getElementsByClassName(''))
+     //removeProperty(this);
+    
+    })
+    //item += document.createElement("button");
     return item
   },
 
   handleSubmit: function(ev) {
     ev.preventDefault()
-
+  
+  
     const f = ev.target
 
     const spell = {
       name: f.spellName.value,
       level: f.level.value,
+      
     }
-
+    arr.push(f.spellName.value);
+    console.log(arr);
     const item = this.renderItem(spell)
 
     const list = document.querySelector('#spells')
