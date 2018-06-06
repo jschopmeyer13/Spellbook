76ec42f4f40a0150886b28362b5864fc8cca1f21
Jacob Schopmeyer <jacob.schopmeyer@gmail.com>
2018-06-06 18:20:51 -0400

    Copied files from classwork today

--- index.js
+++ index.js
@@ -1,66 +1,57 @@
-const form = document.querySelector('form')
+const app = {
+  init: function() {
+    const form = document.querySelector('form')
+    form.addEventListener('submit', ev => { // defining a new function but it is defining this how we expect it
+      this.handleSubmit(ev)
+    })
+  },
 
-// const changeHeading = function(ev) {
-//   ev.preventDefault()
+  renderProperty: function(name, value) {
+    const el = document.createElement('span')
+    el.textContent = value
+    el.classList.add(name)
+    return el
+  },
 
-//   const f = ev.target;
-//   const spellName = f.spellName.value;
-//   const level = f.level.value;
-//   const spellsDiv = document.querySelector('#spells');
+  renderItem: function(spell) {
+    // ['name', 'level']
+    properties = Object.keys(spell)
 
-//   spellsDiv.innerHTML += `<li>${spellName + '- level ' + level}</li>`
+    // collect an array of renderProperty's return values
+    // (an array of <span> elements)
+    const childElements = properties.map(property => {
+      return this.renderProperty(property, spell[property])
+    })
 
-//   f.reset()
-// }
-const addElements = function(ev){
-  ev.preventDefault();
+    const item = document.createElement('li')
+    item.classList.add('spell')
 
-  const f = ev.target;
-  const spellName = f.spellName.value;
-  const level = f.level.value;
+    // append each <span> to the <li>
+    childElements.forEach(el => {
+      item.appendChild(el)
+    })
 
- // let brk = document.createElement('<br>');
-  
-  let spellItem = document.createElement('SPAN');
-  let levelItem = document.createElement('SPAN');
-  //let spanNode = document.createElement('SPAN', spellName)
-  //const currentSpell = document.getElementsByClassName('spellName');
-  //currentSpell.appendChild(node).appendChild(spellName).appendChild(node);
-  spellItem.className = "spellName";
-  levelItem.className = "level";
+    return item
+  },
 
- // let textNode = document.createTextNode(spellName + ' - level ' + level);
-  
-  const spellNode = document.createTextNode(spellName + '   ');
+  handleSubmit: function(ev) {
+    ev.preventDefault()
 
-  const levelNode = document.createTextNode(level);
-  spellItem.appendChild(spellNode);
-  levelItem.appendChild(levelNode);
- // node.appendChild(spanNode);
-  //node.appendChild(textNode);
-  const brk = document.createElement('br');
-  levelItem.appendChild(brk);
-  //node.appendChild(brk)
-  //document.getElementById("types").appendChild(node);
-  document.getElementById('types').appendChild(spellItem);
-  document.getElementById('types').appendChild(levelItem);
-  
-  //document.getElementById("spellName").appendChild(spellNode);
-  
+    const f = ev.target
 
-  f.reset();
+    const spell = {
+      name: f.spellName.value,
+      level: f.level.value,
+    }
+
+    const item = this.renderItem(spell)
+
+    const list = document.querySelector('#spells')
+    list.appendChild(item)
+
+    f.reset()
+  },
 }
-// const addStudents = function(ev){
-//     ev.preventDefault()
 
-//     const f = ev.target
-//     const studentName = f.studentName.value
-    
-//     const studentsDiv = document.querySelector('#students')
-//     studentsDiv.innerHTML += `<li>${studentName}</li>`
-    
-//     f.reset()
-// }
+app.init()
 
-
-form.addEventListener('submit', addElements)
